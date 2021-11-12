d7b0268525f66208b125254ce22bb7fecfc328be
Evgeniy Danilchenko <evgeniy.danilchenko@gmail.com>
2021-11-12 23:00:10 +0100

    Add statistics to Popup.html

--- js/popup.js
+++ js/popup.js
@@ -1,9 +1,23 @@
 function App() {
-  this.InitControllers();
+  this.totalContainer = document.getElementById("totalCounterContainer");
+  this.AddEventListeners();
 }
 
-App.prototype.InitControllers = function () {};
+App.prototype.Init = function () {
+  const self = this;
+  chrome.storage.local.get("squidder", function (result) {
+    const total = result?.squidder?.total || 0;
+    self.totalContainer.innerHTML = total;
+  });
+};
 
-$(function () {
+App.prototype.AddEventListeners = function () {
+  document.getElementById("closeBtn").addEventListener("click", () => {
+    self.close();
+  });
+};
+
+document.addEventListener("DOMContentLoaded", function () {
   const app = new App();
+  app.Init();
 });
